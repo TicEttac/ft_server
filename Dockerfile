f604e67032d079f95cba1ad53c1cd30a13f0bcb6
niels <ashomega@live.fr>
2020-07-13 04:57:24 +0200

    working

--- Dockerfile
+++ Dockerfile
@@ -6,12 +6,22 @@
 #    By: nisauvig <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/02/19 23:20:41 by nisauvig          #+#    #+#              #
-#    Updated: 2020/02/21 19:38:04 by nisauvig         ###   ########.fr        #
+#    Updated: 2020/07/13 04:54:36 by nisauvig         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-FROM debian:buster
+# escape='
 
-RUN	apt-get update						\
-	&& apt-get install -y nginx wget	\
-	&& nginx							\
+FROM debian:buster
+COPY	srcs /root/srcs
+
+RUN	apt-get update													\
+	&& apt-get install -y nginx mariadb-server php-fpm php-mysql	\
+	&& rm /etc/nginx/sites-enabled/default							\
+	&& mv ~/srcs/serve.com /etc/nginx/sites-enabled/				\
+	&& mv ~/srcs/my.cnf /etc/mysql/									\
+	&& mkdir -p /var/run/mysqld										\
+	&& chown mysql /var/run/mysqld									\
+	&& service mysql restart										\
+	&& service nginx restart											\
+	&& mariadb														\
--- docker_start.sh
+++ docker_start.sh
@@ -6,18 +6,23 @@
 #    By: nisauvig <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/02/21 18:50:38 by nisauvig          #+#    #+#              #
-#    Updated: 2020/02/21 19:45:14 by nisauvig         ###   ########.fr        #
+#    Updated: 2020/07/13 02:57:32 by nisauvig         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME=ft_server
 GREEN=\\033[32m
-_ENDL=\\033[0m
 CYAN=\\033[36m
+RED=\\033[31m
+_ENDL=\\033[0m
 
-docker build -t $NAME:$NAME . > /dev/null
-echo "$GREEN Docker image $NAME successfully built$_ENDL"
-echo "$CYAN Launching $NAME image...$_ENDL"
-echo "$CYAN Launching nginx on $NAME$_ENDL"
-docker run -it -p 80:80 -p 443:443 $NAME
-docker rm $NAME
+docker build -t $NAME .
+if [ `echo $?` != 1 ]
+then
+	echo "$GREEN Docker image $NAME successfully built$_ENDL"
+	echo "$CYAN Launching $NAME image...$_ENDL"
+	echo "$CYAN Launching nginx on $NAME$_ENDL"
+	docker run -it -p 80:80 -p 443:443 $NAME
+else
+	echo "$RED Failed to build docker image\n$_ENDL"
+fi
